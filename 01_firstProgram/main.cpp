453a98097387d1e37575199a2fc15f1dd91e91fb
Alessio Nisticò <alenn97@Spyro.Dragons>
2025-02-26 12:40:35 +0100

    minor update

--- 01_firstProgram/main.cpp
+++ 01_firstProgram/main.cpp
@@ -5,7 +5,7 @@ int main() {
 
     std::cout << "Hello, World!" << std::endl;
     std::cout << "This is a basic C++ output." << std::endl;
-
+  
     // Return 0 to indicate that the program has successfully executed
     return 0;
 } 
